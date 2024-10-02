4beb4b03433d455f076ad4768379b262306e186c
Dama Venkata Vamsi <venkatavamsi560@gmail.com>
2024-10-02 15:44:21 +0530

    data collection done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'venkatavamsidama',
+    author_email= 'venkatavamsi560@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
